fa94025621d84e7aa3b4c10e5bac6da8fa189248
Yashvim49 <163328427+Yashvim49@users.noreply.github.com>
2024-06-09 16:42:41 +0530

    Add files via upload

---
+++ person/DemoPerson.java
@@ -0,0 +1,19 @@
+
+package tnsif.region.personalinfo;
+
+import tnsif.region.countrystatepack.City;
+import tnsif.region.countrystatepack.Country;
+
+public class DemoPerson {
+    public static void main(String[] args) {
+        // Create City and Country objects
+        City city = new City("Rajula");
+        Country country = new Country("India");
+
+        // Create a Person object
+        Person person = new Person("Yashvi", city, country);
+
+        // Print the Person object
+        System.out.println(person);
+    }
+}
